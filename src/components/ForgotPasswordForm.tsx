8f8c10af4649dedc0247a9ff552d00c3bd2542ef
hcdiekmann <cdiekmann33@gmail.com>
2024-04-12 20:52:30 +0200

    Add react-icons package and update reset pass page

--- src/components/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { FaEnvelope } from "react-icons/fa";
 import {
     Form,
     FormControl,
@@ -13,6 +14,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@/provider/AuthProvider";
+import { useState } from "react";
 
 const formSchema = z.object({
     email: z.string().email({ message: "Invalid email address." }).min(1, "Email is required."),
@@ -22,6 +24,7 @@ type ForgotPasswordFormInputs = z.infer<typeof formSchema>;
 
 export const ForgotPasswordForm = () => {
     const { resetPassword } = useAuth();
+    const [emailSent, setEmailSent] = useState(false);
     const form = useForm<ForgotPasswordFormInputs>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -35,33 +38,39 @@ export const ForgotPasswordForm = () => {
             console.log(error);
             toast.error('Password reset failed', { duration: 4000, description: `${error.message}` });
         } else {
-           toast.info('Reset email sent', { duration: 5000, description: `Check your email for a password reset link.` });
+            setEmailSent(true);
         }
     };
 
     return (
         <div className="flex flex-col space-y-4 w-full max-w-md mx-auto bg-white p-8 rounded-lg shadow">
-            <h1 className="text-center text-3xl font-extrabold text-[#00431C] mb-4">Reset your password</h1>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                    {/* Email input field */}
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field, fieldState }) => (
-                            <FormItem>
-                                <FormLabel>Account Email</FormLabel>
-                                <FormControl>
-                                    <Input type="email" placeholder="Enter your email" {...field} />
-                                </FormControl>
-                                <FormMessage>{fieldState.error?.message}</FormMessage>
-                            </FormItem>
-                        )}
-                    />
-                    {/* Submit button */}
-                    <Button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md">Send Reset Link</Button>
-                </form>
-            </Form>
+            {!emailSent ? (
+                <>
+                    <h1 className="text-center text-3xl font-extrabold text-[#00431C]">Reset your password</h1>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                            <FormField control={form.control} name="email" render={({ field, fieldState }) => (
+                                <FormItem>
+                                    <FormLabel>Account Email</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" placeholder="Enter your email" {...field} />
+                                    </FormControl>
+                                    <FormMessage>{fieldState.error?.message}</FormMessage>
+                                </FormItem>
+                            )} />
+                            <Button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md">Send Link</Button>
+                        </form>
+                    </Form>
+                </>
+            ) : (
+                <>
+                    <h1 className="text-center text-3xl font-extrabold text-[#00431C]">Password reset link sent</h1>
+                    <div class="flex flex-col items-center justify-center">
+                        <FaEnvelope size={48} color="#00431C" />
+                        <p class="text-lg text-center">Please check your inbox to proceed with resetting your password.</p>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
