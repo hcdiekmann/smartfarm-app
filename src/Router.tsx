89db95ccd6d87d73c92a1d1df75189b5bfcee736
Hans Christian Diekmann <cdiekmann33@gmail.com>
2024-06-02 10:48:42 +0200

    switch to hashrouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import PrivateRoute from './components/PrivateRoute';
@@ -10,7 +10,7 @@ import AuthCallback from './pages/AuthCallback';
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -20,7 +20,7 @@ const Router = () => {
         <Route path="/update-password" element={<UpdatePasswordPage/>} /> {/* Make Private  */}
         <Route path="*" element={<NotFoundPage/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
