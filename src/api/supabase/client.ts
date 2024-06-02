66f0e98ce919a9a37c97546ab14e02c573c19771
Hans Christian Diekmann <cdiekmann33@gmail.com>
2024-06-02 12:47:50 +0200

    auth context refactored

--- src/api/supabase/client.ts
+++ src/api/supabase/client.ts
@@ -2,5 +2,13 @@ import { SupabaseClient, createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// see options https://supabase.com/docs/reference/javascript/initializing
+const supabaseOptions = {
+    auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+    },
+}
          
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey,supabaseOptions);
--- src/provider/AuthProvider.tsx
+++ src/provider/AuthProvider.tsx
@@ -24,67 +24,56 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
+    console.log('AuthProvider: useEffect user:', user);
     const getSession = async () => {
-      const { data: { session }, error } = await supabase.auth.getSession()
-      setUser(session?.user ?? null);
+      const { data: { session }, error } = await supabase.auth.getSession();
       if (error) {
         console.error('Error fetching session:', error.message);
+      } else {
+        setUser(session?.user ?? null);
       }
     };
 
     getSession();
 
-    const { data } = supabase.auth.onAuthStateChange(async (event, session) => {
-
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
       switch (event) {
         case 'INITIAL_SESSION':
-          setUser(session?.user ?? null);
-          break;
         case 'SIGNED_IN':
-          console.log('User signed in');
+        case 'TOKEN_REFRESHED':
+        case 'USER_UPDATED':
           setUser(session?.user ?? null);
           break;
         case 'SIGNED_OUT':
-          console.log('User signed out');
           setUser(null);
           break;
         case 'PASSWORD_RECOVERY':
           console.log('Password recovery mode');
           break;
-        case 'TOKEN_REFRESHED':
-          console.log('Token refreshed');
-          break;
-        case 'USER_UPDATED':
-          console.log('User updated');
-          setUser(session?.user ?? null);
-          break;
       }
     });
 
-    // Clean up the listener on component unmount
     return () => {
-      data.subscription.unsubscribe();
+      authListener?.subscription.unsubscribe();
     };
-
   }, []);
 
   const signUp = async (firstName: string, lastName: string, email: string, password: string) => {
     const authResponse = await supabase.auth.signUp({ email, password });
     if (authResponse.data.user) {
-      await setUserName(firstName, lastName); //TODO: fix this, cant update user if not auto confirmed
+      await setUserName(firstName, lastName); // TODO: fix this, cant update user if not auto confirmed
     }
     return authResponse;
-  }
+  };
 
   const signInWithGoogle = async () => {
     return await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `https://app.smartfarm.ing/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
   };
-
 
   const setUserName = async (firstName: string, lastName: string) => {
     return await supabase.auth.updateUser({
@@ -92,27 +81,26 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         first_name: firstName,
         last_name: lastName,
       },
-    })
-  }	
-
-    const value = {
-      user,
-      signUp: signUp,
-      signIn: (email: string, password: string) => supabase.auth.signInWithPassword({ email, password }),
-      signInWithGoogle,
-      signOut: () => supabase.auth.signOut(),
-      resetPassword: (email: string) => supabase.auth.resetPasswordForEmail(email, { redirectTo: '/update-password' }),
-      updatePassword: (newPassword: string) => supabase.auth.updateUser({ password: newPassword }),
-    };
-
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    });
   };
 
-  // Custom hook to use auth context
-  export const useAuth = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (context === undefined) {
-      throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const value = {
+    user,
+    signUp,
+    signIn: (email: string, password: string) => supabase.auth.signInWithPassword({ email, password }),
+    signInWithGoogle,
+    signOut: () => supabase.auth.signOut(),
+    resetPassword: (email: string) => supabase.auth.resetPasswordForEmail(email, { redirectTo: '/update-password' }),
+    updatePassword: (newPassword: string) => supabase.auth.updateUser({ password: newPassword }),
   };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
